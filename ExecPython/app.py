a288b5bf35f08956e114a99f8afb84959ae849aa
ksergeyru <i@ksergey.ru>
2022-08-30 01:07:49 +0300

    Переменная как строка и функция exec в Python

---
+++ ExecPython/app.py
@@ -0,0 +1,20 @@
+tests = ["goo", "doo", "foo"]
+locals()[tests[0]] = 42
+print(f"goo = {goo}")  # goo = 42
+
+# 2.
+locals()[tests[1]] = 2809
+print(f"doo = {doo}")  # doo = 2809
+
+value = 2023
+exec(f"{tests[2]} = {value}")
+print(f"foo = {foo}")  # foo = 2023
+
+code = '''
+a = 2
+b = 3
+c = f"{a} + {b} = {a+b}"
+print(c)
+'''
+print(code)
+exec(code)  # 2 + 3 = 5
