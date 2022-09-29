bd72d9f4e66a6a37eb30692d9c0ea5386854c273
ksergeyru <i@ksergey.ru>
2022-09-30 02:03:43 +0300

    Пример реализации стека только с push и pop

---
+++ StackExample/Ex02_SimpleStack/Program.cs
@@ -0,0 +1,23 @@
+﻿// const int size = 5;
+
+// char[] storage = new char[size];
+// int top = 0;
+// void PUSH(char x, char[] S) => S[top++] = x;
+// char POP(char[] S) => S[--top];
+
+// // Демонстрация работы
+// PUSH('t', storage);
+// char item = POP(storage);
+// Console.WriteLine($"item: {item}");
+
+const int size = 5;
+
+char[] stack = new char[size];
+int top = 0;
+void PUSH(char x) => stack[top++] = x;
+char POP() => stack[--top];
+
+// Демонстрация работы
+PUSH('t');
+char item = POP();
+Console.WriteLine($"item: {item}");
